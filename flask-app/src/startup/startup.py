5b51942ea6dfcc4182fc0ace5c11d8461801811c
thaddeuskopczynski <kopczynski.t@northeastern.edu>
2024-04-17 21:17:40 -0400

    added startupID get req

--- flask-app/src/startup/startup.py
+++ flask-app/src/startup/startup.py
@@ -31,6 +31,31 @@ def get_startup():
 
     return jsonify(json_data)
 
+# Get all the products from the database
+@startup.route('/startupID', methods=['GET'])
+def get_startupID():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+
+    cursor.execute('SELECT StartupID FROM Startup')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
 
 
 @startup.route('/startup', methods=['POST'])
